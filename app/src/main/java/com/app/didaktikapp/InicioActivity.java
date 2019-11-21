7a5c670da32e854e534178e6a7f36e83c4626638
Asier97 <asiersuarez97@gmail>
2019-11-21 10:51:28 +0100

    Merge branch 'branch1' of /home/dm2/AndroidStudioProjects/Didaktikapp with conflicts.

---
+++ app/src/main/java/com/app/didaktikapp/InicioActivity.java
@@ -0,0 +1,14 @@
+package com.app.didaktikapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InicioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio);
+    }
+}
