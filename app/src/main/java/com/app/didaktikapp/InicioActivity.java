eb212c10fe78465348504842fc2810f3ec60e3aa
Asier97 <asiersuarez97@gmail>
2019-11-21 13:55:03 +0100

    Imagenes de CJ y UPV añadidas. Pantalla de inicio añadida, necesita un retoque Se muestra y se cierra sola Dejo los comentarios de las pruebas que he hecho

--- app/src/main/java/com/app/didaktikapp/InicioActivity.java
+++ app/src/main/java/com/app/didaktikapp/InicioActivity.java
@@ -3,6 +3,7 @@ package com.app.didaktikapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class InicioActivity extends AppCompatActivity {
 
@@ -10,5 +11,31 @@ public class InicioActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
+
+//        ImageView logo = findViewById(R.id.ivLogo);
+//
+//        logo.setImageResource(R.drawable.espanita);
+
+//        try {
+//            Thread.sleep(4000);
+//        } catch (InterruptedException e) {
+//            Log.i("Error","Esperando a cerrar la pantalla de inicio");
+//        } finally {
+//            finish();
+//        }
+
+        Thread splashTread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(4000);
+                } catch (InterruptedException e) {
+                    Log.i("Error", "Esperando a cerrar la pantalla de inicio");
+                }
+                finish();
+            }
+        };
+        splashTread.start();
+
     }
 }
