13a5094467167813c184b194e3b37264cf2252f2
gennakk <ralonsobeas@gmail.com>
2019-11-27 08:20:15 +0100

    Coninuación del puzle

--- app/src/main/java/com/app/didaktikapp/Fragments/FragmentTren.java
+++ app/src/main/java/com/app/didaktikapp/Fragments/FragmentTren.java
@@ -2,7 +2,12 @@ package com.app.didaktikapp.Fragments;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
@@ -198,6 +203,81 @@ public class FragmentTren extends Fragment {
                 piece.yCoord = yCoord - offsetY + imageView.getTop();
                 piece.pieceWidth = pieceWidth + offsetX;
                 piece.pieceHeight = pieceHeight + offsetY;
+
+                // this bitmap will hold our final puzzle piece image
+                Bitmap puzzlePiece = Bitmap.createBitmap(pieceWidth + offsetX, pieceHeight + offsetY, Bitmap.Config.ARGB_8888);
+
+                // draw path
+                int bumpSize = pieceHeight / 4;
+                Canvas canvas = new Canvas(puzzlePiece);
+                Path path = new Path();
+                path.moveTo(offsetX, offsetY);
+                if (row == 0) {
+                    // top side piece
+                    path.lineTo(pieceBitmap.getWidth(), offsetY);
+                } else {
+                    // top bump
+                    path.lineTo(offsetX + (pieceBitmap.getWidth() - offsetX) / 3, offsetY);
+                    path.cubicTo(offsetX + (pieceBitmap.getWidth() - offsetX) / 6, offsetY - bumpSize, offsetX + (pieceBitmap.getWidth() - offsetX) / 6 * 5, offsetY - bumpSize, offsetX + (pieceBitmap.getWidth() - offsetX) / 3 * 2, offsetY);
+                    path.lineTo(pieceBitmap.getWidth(), offsetY);
+                }
+
+                if (col == cols - 1) {
+                    // right side piece
+                    path.lineTo(pieceBitmap.getWidth(), pieceBitmap.getHeight());
+                } else {
+                    // right bump
+                    path.lineTo(pieceBitmap.getWidth(), offsetY + (pieceBitmap.getHeight() - offsetY) / 3);
+                    path.cubicTo(pieceBitmap.getWidth() - bumpSize,offsetY + (pieceBitmap.getHeight() - offsetY) / 6, pieceBitmap.getWidth() - bumpSize, offsetY + (pieceBitmap.getHeight() - offsetY) / 6 * 5, pieceBitmap.getWidth(), offsetY + (pieceBitmap.getHeight() - offsetY) / 3 * 2);
+                    path.lineTo(pieceBitmap.getWidth(), pieceBitmap.getHeight());
+                }
+
+                if (row == rows - 1) {
+                    // bottom side piece
+                    path.lineTo(offsetX, pieceBitmap.getHeight());
+                } else {
+                    // bottom bump
+                    path.lineTo(offsetX + (pieceBitmap.getWidth() - offsetX) / 3 * 2, pieceBitmap.getHeight());
+                    path.cubicTo(offsetX + (pieceBitmap.getWidth() - offsetX) / 6 * 5,pieceBitmap.getHeight() - bumpSize, offsetX + (pieceBitmap.getWidth() - offsetX) / 6, pieceBitmap.getHeight() - bumpSize, offsetX + (pieceBitmap.getWidth() - offsetX) / 3, pieceBitmap.getHeight());
+                    path.lineTo(offsetX, pieceBitmap.getHeight());
+                }
+
+                if (col == 0) {
+                    // left side piece
+                    path.close();
+                } else {
+                    // left bump
+                    path.lineTo(offsetX, offsetY + (pieceBitmap.getHeight() - offsetY) / 3 * 2);
+                    path.cubicTo(offsetX - bumpSize, offsetY + (pieceBitmap.getHeight() - offsetY) / 6 * 5, offsetX - bumpSize, offsetY + (pieceBitmap.getHeight() - offsetY) / 6, offsetX, offsetY + (pieceBitmap.getHeight() - offsetY) / 3);
+                    path.close();
+                }
+
+                // mask the piece
+                Paint paint = new Paint();
+                paint.setColor(0XFF000000);
+                paint.setStyle(Paint.Style.FILL);
+
+                canvas.drawPath(path, paint);
+                paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+                canvas.drawBitmap(pieceBitmap, 0, 0, paint);
+
+
+                // draw a white border
+                Paint border = new Paint();
+                border.setColor(0X80FFFFFF);
+                border.setStyle(Paint.Style.STROKE);
+                border.setStrokeWidth(8.0f);
+                canvas.drawPath(path, border);
+
+                // draw a black border
+                border = new Paint();
+                border.setColor(0X80000000);
+                border.setStyle(Paint.Style.STROKE);
+                border.setStrokeWidth(3.0f);
+                canvas.drawPath(path, border);
+
+                // set the resulting bitmap to the piece
+                piece.setImageBitmap(puzzlePiece);
 
                 pieces.add(piece);
                 xCoord += pieceWidth;
