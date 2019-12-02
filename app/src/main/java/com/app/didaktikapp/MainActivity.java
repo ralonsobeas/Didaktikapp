214819ffce7df682d3a7650dabcd9f239a83ab8d
gennakk <ralonsobeas@gmail.com>
2019-12-02 20:06:16 +0100

    Cambio menú principal

--- app/src/main/java/com/app/didaktikapp/MainActivity.java
+++ app/src/main/java/com/app/didaktikapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.app.didaktikapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.content.res.ResourcesCompat;
@@ -8,11 +9,17 @@ import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.text.Html;
+import android.util.Log;
+import android.view.Display;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.TranslateAnimation;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.app.didaktikapp.Activities.MapActivity;
+import com.app.didaktikapp.CircleMenu.CircleMenuView;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -21,6 +28,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 import at.markushi.ui.CircleButton;
+import pl.droidsonroids.gif.GifImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -58,21 +66,87 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-        layout = findViewById(R.id.layout);
+//        layout = findViewById(R.id.layout);
+//
+//        int[] fondos = {R.drawable.login1,
+//                        R.drawable.login2,
+//                        R.drawable.login3,
+//                        R.drawable.login4};
+//
+//        int num = (int) (Math.random()*fondos.length);
+//        layout.setBackgroundResource(fondos[num]);
 
-        int[] fondos = {R.drawable.login1,
-                        R.drawable.login2,
-                        R.drawable.login3,
-                        R.drawable.login4};
+//        botonSalir = findViewById(R.id.btnSalir);
+//        botonInicio = findViewById(R.id.btnIniciar);
+//        botonContinuar = findViewById(R.id.btnContinuar);
+//
+//        eventos();
 
-        int num = (int) (Math.random()*fondos.length);
-        layout.setBackgroundResource(fondos[num]);
+        final CircleMenuView menu = findViewById(R.id.circle_menu);
+        menu.setEventListener(new CircleMenuView.EventListener() {
+            @Override
+            public void onMenuOpenAnimationStart(@NonNull CircleMenuView view) {
+                Log.d("D", "onMenuOpenAnimationStart");
+            }
 
-        botonSalir = findViewById(R.id.btnSalir);
-        botonInicio = findViewById(R.id.btnIniciar);
-        botonContinuar = findViewById(R.id.btnContinuar);
+            @Override
+            public void onMenuOpenAnimationEnd(@NonNull CircleMenuView view) {
+                Log.d("D", "onMenuOpenAnimationEnd");
+            }
 
-        eventos();
+            @Override
+            public void onMenuCloseAnimationStart(@NonNull CircleMenuView view) {
+                Log.d("D", "onMenuCloseAnimationStart");
+            }
+
+            @Override
+            public void onMenuCloseAnimationEnd(@NonNull CircleMenuView view) {
+                Log.d("D", "onMenuCloseAnimationEnd");
+            }
+
+            @Override
+            public void onButtonClickAnimationStart(@NonNull CircleMenuView view, int index) {
+                Log.d("D", "onButtonClickAnimationStart| index: " + index);
+
+            }
+
+            @Override
+            public void onButtonClickAnimationEnd(@NonNull CircleMenuView view, int index) {
+                Log.d("D", "onButtonClickAnimationEnd| index: " + index);
+                switch (index){
+                    case 0:
+                        Intent i = new Intent(MainActivity.this, MapActivity.class);
+                        startActivity(i);
+                        break;
+                    case 1:
+                        break;
+                    case 2:
+                        finish();
+                        break;
+                }
+
+            }
+
+            @Override
+            public boolean onButtonLongClick(@NonNull CircleMenuView view, int index) {
+                Log.d("D", "onButtonLongClick| index: " + index);
+                return true;
+            }
+
+            @Override
+            public void onButtonLongClickAnimationStart(@NonNull CircleMenuView view, int index) {
+                Log.d("D", "onButtonLongClickAnimationStart| index: " + index);
+            }
+
+            @Override
+            public void onButtonLongClickAnimationEnd(@NonNull CircleMenuView view, int index) {
+                Log.d("D", "onButtonLongClickAnimationEnd| index: " + index);
+            }
+        });
+
+        GifImageView gifImageView = findViewById(R.id.gif);
+
+
 
     }
 
@@ -95,19 +169,19 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 //PRUEBA PARA PYTHON
-
-                try {
-                    String prg = "import sys";
-                    BufferedWriter out = new BufferedWriter(new FileWriter("path/a.py"));
-                    out.write(prg);
-                    out.close();
-                    Process p = Runtime.getRuntime().exec("python path/a.py");
-                    BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
-                    String ret = in.readLine();
-                    System.out.println("value is : "+ret);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+//
+//                try {
+//                    String prg = "import sys";
+//                    BufferedWriter out = new BufferedWriter(new FileWriter("path/a.py"));
+//                    out.write(prg);
+//                    out.close();
+//                    Process p = Runtime.getRuntime().exec("python path/a.py");
+//                    BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
+//                    String ret = in.readLine();
+//                    System.out.println("value is : "+ret);
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
 
             }
         });
